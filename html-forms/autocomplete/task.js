244cb69f4b40de1a2a26f481c6cf85b9711478f8
Kolycheva Anna <annakolyt@gmail.com>
2020-11-17 15:46:31 +0700

    first commit autocomplete

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -67,27 +67,22 @@ class Autocomplete {
     this.list.innerHTML = html.join('');
   }
 
-  getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
+  getMatches(text) {
 
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
-      {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
-      }
-    */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
+      const options = Array.from(this.input.options);
+      const result = [];
+      options.forEach(element => {
+          if (element.innerText.includes(text)) {
+              let textObj = {
+                  text: element.innerText,
+                  value: element.value
+              }
+              result.push(textObj);
+          }
+      })
+      return result
   }
-}
+  
+  }
 
 new Autocomplete( document.querySelector( '.autocomplete' ));
