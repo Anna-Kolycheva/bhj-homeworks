7379129a14d718875f40e4f8f9dabc450cf75428
Kolycheva Anna <annakolyt@gmail.com>
2020-11-28 17:36:11 +0700

    second commit

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,18 +1,55 @@
 const interests = document.querySelector('.interests_main');
 
 interests.addEventListener('change', event => {
-    let check = event.target.closest('.interest');
+    const check = event.target.closest('.interest');
     Array.from(check.getElementsByClassName('interest__check')).forEach(element => {
+        element.indeterminate = false;
         event.target.checked ? element.checked = true : element.checked = false
     })
 
-    let parentElements = Array.from(check.parentElement.getElementsByClassName('interest__check'));
-    let checkedItems = parentElements.filter(item => item.checked);
-    let parentItems = check.parentElement.closest('.interest').querySelector('.interest__check');
+    const parentElements = Array.from(check.parentElement.getElementsByClassName('interest__check'));
+    const checkedItems = parentElements.filter(item => item.checked);
 
-    if (parentElements.length === checkedItems.length) {
-        parentItems.checked = true;
-    } else {
-        parentItems.checked = false;
+    if (check.parentElement.closest('.interest') !== null) {
+
+        const parentItems = check.parentElement.closest('.interest').querySelector('.interest__check');
+
+        if (parentElements.length === checkedItems.length) {
+            parentItems.indeterminate = false;
+            parentItems.checked = true;
+            checkParentOfParent()
+        } else {
+            parentItems.checked = false;
+            if (checkedItems.length !== 0) {
+                parentItems.indeterminate = true;
+                indeterminateParentOfParent(true);
+
+            } else {
+                parentItems.indeterminate = false;
+                indeterminateParentOfParent(false);
+                checkParentOfParent();
+            }
+        }
+
+        function indeterminateParentOfParent(value) {
+            checkParentOfParent()
+            if (parentItems.closest('ul').closest('li') !== null) {
+                parentItems.closest('ul').closest('li').querySelector('.interest__check').indeterminate = value;
+            }
+        }
+
+        function checkParentOfParent() {
+            if (parentItems.closest('ul').closest('li') !== null) {
+                const df = Array.from(parentItems.closest('ul').closest('li').getElementsByClassName('interest__check'))
+                const checkedItemsdf = df.filter(item => item.checked);
+                const parentOfParent = parentItems.closest('ul').closest('li').querySelector('.interest__check');
+                if (df.length - 1 === checkedItemsdf.length) {
+                    parentOfParent.indeterminate = false;
+                    parentOfParent.checked = true;
+                } else {
+                    parentOfParent.checked = false;
+                }
+            }
+        }
     }
 })
